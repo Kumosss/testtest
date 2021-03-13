8bb67f77624e51833693cebc57df1d80413f8cf5
Kumosss <mateusz.kumosinski@gmail.com>
2021-03-13 15:18:14 +0100

    juz

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const express = require('express')
-
-
+const config = require('config')
 const app = express()
-const port = 8080
+const port = process.env.PORT || config.get('port')
 
 app.listen(port, () => console.log(`server listen at ${port}`))
