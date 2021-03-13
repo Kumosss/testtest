e2cb98957c09fccf1db7f133f1ca648904b1c941
Kumosss <mateusz.kumosinski@gmail.com>
2021-03-13 16:03:21 +0100

    bum

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const express = require('express')
 const config = require('config')
-const app = express()
+
+const app = require('./src/app')
+
 const port = process.env.PORT || config.get('port')
 
 app.listen(port, () => console.log(`server listen at ${port}`))
